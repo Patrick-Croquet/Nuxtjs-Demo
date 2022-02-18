4110b140a5745b74a8e539dac57e394b4348594e
Patrick-Croquet <58164643+Patrick-Croquet@users.noreply.github.com>
2022-02-18 08:55:45 +0100

    Initial commit

--- real-world-nuxt/nuxt.config.js
+++ real-world-nuxt/nuxt.config.js
@@ -1,15 +1,14 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'NuxtJs-Blog',
+    title: 'real-world-nuxt',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { hid: 'description', name: 'description', content: '' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -29,12 +28,12 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    // https://go.nuxtjs.dev/bootstrap
+    'bootstrap-vue/nuxt',
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
---
+++ real-world-nuxt/pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1 class="red">{{ title }}</h1>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        title: "Page d'accueil"
+      }
+    },
+    head() {
+      return {
+        title: this.title,
+        meta: [
+          {
+            hid: 'description',
+            name: 'description',
+            content: "Description de la page d'accueil"
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style>
+  .red {
+    color: red;
+  }
+</style>
